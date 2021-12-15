49c1cbc5d756f157ebdfef14ce7406c2edd7c28c
krunalex <krunalex@kkg-do.eu>
2021-12-15 12:09:30 +0100

    Changes by fielgeor

--- src/main/java/my_project/model/PowerUp.java
+++ src/main/java/my_project/model/PowerUp.java
@@ -3,8 +3,14 @@ package my_project.model;
 import KAGO_framework.model.GraphicalObject;
 
 public class PowerUp extends GraphicalObject {
-    
-    public PowerUp(){
+    private int speedBuff;
+    private int increaseShootSpeedBuff;
+    private int increaseShootDamage;
+    private boolean invincible;
+    private int addLife;
+    private int temporaryAddLife;
+
+    public PowerUp(int speedBuff, int increaseShootSpeedBuff){
         
     }
 }
