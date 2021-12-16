6f8083c85cf20d86bd575b15942f864fd494f920
balzlore <balzlore@kkg-do.eu>
2021-12-16 08:42:44 +0100

    Die Zähnen haben jetzt eigene Klassen (und funktionieren jetzt scheinbar auch tatsächlich)

---
+++ src/main/java/my_project/view/MainMenu.java
@@ -0,0 +1,28 @@
+package my_project.view;
+
+import KAGO_framework.control.ViewController;
+import KAGO_framework.view.DrawTool;
+import my_project.control.GraphicalWindow;
+
+import java.awt.*;
+
+public class MainMenu extends GraphicalWindow {
+
+    public MainMenu(ViewController viewController) {
+        super();
+        viewController.createScene();
+    }
+
+    @Override
+    public void draw(DrawTool drawTool){
+        drawTool.setCurrentColor(Color.white);
+        drawTool.formatText("Comic Sans MS",Font.BOLD,20);
+        drawTool.drawText(540,900,"OMG DAS BESTE SPIEL DER WELT: SPACE OUTVADERS");
+        drawTool.drawText(150,220, "Start");
+        drawTool.drawRectangle(125,200,150,100);
+        drawTool.drawText(475,220, "Ende");
+        drawTool.drawRectangle(450,200,150,100);
+        drawTool.drawText(775,220, "Optionen");
+        drawTool.drawRectangle(750,200,150,100);
+    }
+}
