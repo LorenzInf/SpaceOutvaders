90b8817d0a5c5ffc2d05b568cc1fe42a7b858a67
balzlore <balzlore@kkg-do.eu>
2022-01-12 12:49:07 +0100

    Main menu pngs preloaded

--- src/main/java/my_project/view/MainMenu.java
+++ src/main/java/my_project/view/MainMenu.java
@@ -5,25 +5,36 @@ import KAGO_framework.view.DrawTool;
 import my_project.control.GraphicalWindow;
 import my_project.control.ProgramController;
 
+import java.awt.image.BufferedImage;
+
 public class MainMenu extends GraphicalWindow {
 
     private ProgramController programController;
+    private BufferedImage[] images;
 
     public MainMenu(ViewController viewController, ProgramController programController) {
         super();
         this.programController = programController;
         viewController.createScene();
+        images = new BufferedImage[]{
+                load("main_screen.png"), //0
+                load("main_screen_settings_selected.png"), //1
+                load("main_screen_play_selected.png"), //2
+                load("main_screen_leaderboard_selected.png"), //3
+                load("main_screen_question_mark_selected"), //4
+                load("x_selected.png") //5
+        };
     }
 
     @Override
     public void draw(DrawTool drawTool){
         drawTool.drawImage(load("main_screen.png"), 0 ,0);
         switch(programController.getWindow().getMainMenuIndex()){
-            case 1 -> drawTool.drawImage(load("main_screen_settings_selected.png"), 0 ,0);
-            case 2 -> drawTool.drawImage(load("main_screen_play_selected.png"), 0 ,0);
-            case 3 -> drawTool.drawImage(load("main_screen_leaderboard_selected.png"), 0 ,0);
-            case 4 -> drawTool.drawImage(load("main_screen_question_mark_selected.png"), 0 ,0);
-            case 5 -> drawTool.drawImage(load("x_selected.png"), 0 ,0);
+            case 1 -> drawTool.drawImage(images[0], 0 ,0);
+            case 2 -> drawTool.drawImage(images[1], 0 ,0);
+            case 3 -> drawTool.drawImage(images[2], 0 ,0);
+            case 4 -> drawTool.drawImage(images[3], 0 ,0);
+            case 5 -> drawTool.drawImage(images[4], 0 ,0);
         }
     }
 }
