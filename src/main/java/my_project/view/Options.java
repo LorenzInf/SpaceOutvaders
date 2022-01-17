5eba421a915e0e6801529dd8e6eb5e08d9d7e54e
LorenzInf <balzlore@kkg-do.eu>
2022-01-17 17:37:11 +0100

    what nils said

--- src/main/java/my_project/view/Options.java
+++ src/main/java/my_project/view/Options.java
@@ -39,14 +39,14 @@ public class Options extends GraphicalWindow {
         drawTool.drawImage(images[0], 0 ,0);
 
         // Wenn es neu gezeichnet wird muss geguckt werden, ob es an oder aus gemacht werden muss
-        drawTool.drawImage(musicOn ? images[1] : images[2], 0, 0);
+        drawTool.drawImage(images[musicOn ? 1 : 2], 0, 0);
 
         // Wenn es neu gezeichnet wird muss geguckt werden, ob es an oder aus gemacht werden muss
-        drawTool.drawImage(soundOn ? images[3] : images[4], 0, 0);
+        drawTool.drawImage(images[soundOn ? 3 : 4], 0, 0);
 
         switch(programController.getWindow().getOptionsIndex()) { // Symbol markiert oder nicht
-            case 1 -> drawTool.drawImage(musicOn ? images[5] : images[6], 0, 0);
-            case 2 -> drawTool.drawImage(soundOn ? images[7] : images[8], 0, 0);
+            case 1 -> drawTool.drawImage(images[musicOn ? 5 : 6], 0, 0);
+            case 2 -> drawTool.drawImage(images[soundOn ? 7 : 8], 0, 0);
             case 3 -> drawTool.drawImage(images[9], 0, 0);
         }
     }
